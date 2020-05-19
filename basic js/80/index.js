960ce6f96fd11c6e1f5e542163825d73b9b7fd9c
Alessandra Targino <targinoale@gmail.com>
2020-05-19 13:50:50 -0300

    bjs80

---
+++ basic js/80/index.js
@@ -0,0 +1,10 @@
+var myDog = {
+    // Only change code below this line
+      "name": "Luna",
+      "legs": 4,
+      "tails": 1,
+      "friends": ["Frederico", "Belka"]
+    
+    // Only change code above this line
+    };
+    
